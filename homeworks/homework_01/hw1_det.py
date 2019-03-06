43fbf9c140972e884e84dd215b426d5fd82db915
Slovvturtle <48096589+Slovvturtle@users.noreply.github.com>
2019-03-06 23:24:22 +0300

    det

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,22 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    leng = len(list_of_lists)
+    a = 0
+    b = 1
+    ind = 0
+    for j in range(leng):
+        if len(list_of_lists[ind]) != leng:
+            return None
+        minors = []
+        for k in range(leng - 1):
+            minors.append([])
+            for n in range(leng):
+                if j != n:
+                    minors[k].append(list_of_lists[k + 1][n])
+
+        a += b * list_of_lists[ind][j] \
+            * (1 or calculate_determinant(minors))
+        b = -b
+
+    return a

0d8e6d8fb6cb458cd76080f328d68e6598cdde34
Slovvturtle <48096589+Slovvturtle@users.noreply.github.com>
2019-03-06 20:49:13 +0300

    d

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,7 +3,6 @@
 
 
 def find_indices(input_list, n):
-    a = {}
     '''
     Метод возвращает индексы двух различных
     элементов listа, таких, что сумма этих элементов равна
@@ -13,7 +12,8 @@ def find_indices(input_list, n):
     :param input_list: список произвольной длины целых чисел
     :param n: целевая сумма
     :return: tuple из двух индексов или None
-    '''   
+    '''  
+    a = {}
     for i in range(0, len(input_list)):
         if n - input_list[i] in a:
             return (a[n - input_list[i]], i)

ee949b96d10c69ac73dcb076cb21b29ed72e8700
Slovvturtle <48096589+Slovvturtle@users.noreply.github.com>
2019-03-06 20:25:49 +0300

    d

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -10,6 +10,7 @@ def find_indices(input_list, n):
         elif input_list[i] not in a:
             a[input_list[i]] = i
     return None
+
     '''
     Метод возвращает индексы двух различных
     элементов listа, таких, что сумма этих элементов равна
